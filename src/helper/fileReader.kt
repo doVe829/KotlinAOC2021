04cad5ee3fe9cccf61412603e1740607c23ba568
doVe829 <dvennegeerts@gmail.com>
2021-12-03 18:02:31 +0100

    init

---
+++ src/helper/fileReader.kt
@@ -0,0 +1,11 @@
+package helper
+
+import java.io.File
+
+fun fileToIntList(pathName: String): List<Int> {
+    return File(pathName).readLines().map { it.toInt() }
+}
+
+fun fileToList(pathName: String): List<String> {
+    return File(pathName).readLines()
+}
